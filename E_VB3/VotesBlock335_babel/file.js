63f9e8f5989e2aa6cd3d4ef8719e5e993a1b73ca
loktev <loktev@tut.by>
2022-06-25 12:23:50 +0300

    minor

--- E_VB3/VotesBlock335_babel/file.js
+++ E_VB3/VotesBlock335_babel/file.js
@@ -4,3 +4,12 @@ const AAA=111;
 let sum=(a,b) => a+b;
 
 console.log( sum(AAA,222) );
+
+/*
+async function aaa() {
+    for ( let a=0; a<3; a++ ) {
+        console.log(a);
+        await fetch("url"+a);
+    }
+}
+*/
