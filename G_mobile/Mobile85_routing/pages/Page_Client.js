0587a36a2ee790ee1556594b3a5acd125740e078
loktev <loktev@tut.by>
2022-07-16 17:06:48 +0300

    useParams

--- G_mobile/Mobile85_routing/pages/Page_Client.js
+++ G_mobile/Mobile85_routing/pages/Page_Client.js
@@ -5,23 +5,17 @@ import MobileClientInfo from '../components/MobileClientInfo';
 
 import appData from '../appData';
 
-// react-router v6 предоставляет возможности доступа к параметрам из УРЛа только в виде хуков, т.е. для функциональных компонентов
-// эта HOF делает возможным использование этого react-router и с классовыми компонентами
-const withRouter = Component => props => {
-  const params = useParams();
-  return <Component {...props} params={params} /> ;
-};
+// react-router v6 предоставляет возможности доступа к параметрам из УРЛа только через хук useParams,
+// т.е. только для функциональных компонентов
 
-class Page_Client extends React.PureComponent {
-          
-  render() {
-
-    //console.log(this.props);
-
-    // раз написано <Route path="/client/:clid" element={<Page_Client/>} />
-    // значит withRouter(Page_Client) получит то что в УРЛе после /client/ под именем props.params.clid в виде строки
+const Page_Client = props => {
     
-    let clientId=parseInt(this.props.params.clid);
+    const params = useParams();
+    console.log(params);
+    // раз написано <Route path="/client/:clid" element={<Page_Client/>} />
+    // значит Page_Client получит то что в УРЛе после /client/ от хука useParams под именем clid в виде строки
+    
+    let clientId=parseInt(params.clid);
 
     let clientData=appData.clientsArr.find( c => c.id==clientId );
 
@@ -31,8 +25,6 @@ class Page_Client extends React.PureComponent {
       />
     );
     
-  }
-
 }
     
-export default withRouter(Page_Client);
+export default Page_Client;
